dbebc42e13e6c04860b745078b8a325af074de24
luzinsan <luzinsan.a.430-2@e.tusur.ru>
2024-05-17 13:58:38 +0700

    рефакторинг TrainParamsNode

--- nodes/dataset.py
+++ nodes/dataset.py
@@ -1,20 +1,7 @@
-
-import dearpygui.dearpygui as dpg
-from torch import nn
-import torch.optim
-import torch.nn.functional as F
-
 from core.node import Node
 from core.output_node_attr import OutputNodeAttribute
-
 from core.link_node import LinkNode
-from core import utils
-
-from app.pipeline import Pipeline
 from nodes.train_params_node import TrainParamsNode
-from lightning import LightningDataModule
-import os
-from torch.utils.data import DataLoader
 
 
 
@@ -36,32 +23,9 @@ class DataNode(Node):
 
     def _submit(self, parent):
         super()._submit(parent)
-        losses = {"MSE (squared L2)": nn.MSELoss, "Cross Entropy Loss": F.cross_entropy, "L1 Loss": nn.L1Loss}
-        optimizers = {"SGD": torch.optim.SGD, "Adam":torch.optim.Adam, "Adadelta":torch.optim.Adadelta, "Adamax":torch.optim.Adamax}
-        init_params = {'Default':None, 'Normal': utils.init_normal, 'Xavier': utils.init_xavier } 
-
-
-        self.train_params: TrainParamsNode = TrainParamsNode('Train Params',
-                                       train_params=(
-                                            {"label":"Project name", "type":'text', "default_value":'Project', "width":150},
-                                            {"label":"Task name", "type":'text', "default_value":'Experiment', "width":150},
-                                            {"label":"Loss", "type":'combo', "default_value":self._default_params['Loss'], "width":150,
-                                             "items":tuple(losses.keys()), "user_data":losses},
-                                            {"label":"Optimizer", "type":'combo', "default_value":self._default_params['Optimizer'], "width":150,
-                                             "items":tuple(optimizers.keys()), "user_data":optimizers},
-                                            
-                                            {"label":"Initialization", "type":'combo', "default_value":'Xavier', "width":150,
-                                             "items":tuple(init_params.keys()), "user_data":init_params},
-                                            
-                                            {"label":"Learning Rate", "type":'float', "default_value":0.05, "width":150},
-                                            {"label":"Max Epoches", "type":'int', "default_value":2, "width":150},
-                                            {"label":"Save Weights", "type":"file", "callback":Pipeline.save_weight},
-                                            {"label":"Load Weights", "type":"file", "callback":Pipeline.load_weight},
-                                            {"label":"Train", "type":"button", "callback":Pipeline.flow, "user_data":self},
-                                            {"label":"Continue Train", "type":"button", "callback":Pipeline.keep_train, "user_data":self}
-                                       ),
-                                       pos=(100, 250))
         
+        self.train_params: TrainParamsNode = TrainParamsNode('Train Params', data_node=self, default_params=self._default_params, pos=(100, 250))
         self.train_params._submit(parent)
+        
         LinkNode._link_callback(parent, (self._output_attributes[2]._uuid, self.train_params._input_attributes[0]._uuid))
 
--- nodes/train_params_node.py
+++ nodes/train_params_node.py
@@ -1,21 +1,78 @@
+from typing import Any, Optional
+from torch import nn
+import torch.optim
+import torch.nn.functional as F
+
 from core.node import Node
 from core.input_node_attr import InputNodeAttribute
+from core import utils
+
+from app.pipeline import Pipeline
+
+
 
 
 class TrainParamsNode(Node):
+    
+    WIDTH=150
+    __params: dict[str, dict[str, Any]] = dict(
+        Loss= 
+            {"MSE (squared L2)": nn.MSELoss, 
+            "Cross Entropy Loss": F.cross_entropy, 
+            "L1 Loss": nn.L1Loss},
+        Optimizer=
+            {"SGD":torch.optim.SGD, 
+            "Adam":torch.optim.Adam, 
+            "Adadelta":torch.optim.Adadelta, 
+            "Adamax":torch.optim.Adamax},
+        Initialization=
+            {'Default':None, 
+            'Normal': utils.init_normal, 
+            'Xavier': utils.init_xavier},
+    )
+
 
     @staticmethod
-    def factory(name, data=None, train_params: tuple[dict]=None, **node_params):
-        node = TrainParamsNode(name, data, train_params, **node_params)
+    def factory(name, data_node, default_params: Optional[dict[str, str]]=None, **node_params):
+        node = TrainParamsNode(name, data_node, default_params, **node_params)
         return node
 
-    def __init__(self, label: str, data=None, train_params: tuple[dict]=None, **node_params):
-        super().__init__(label, data, **node_params)
+    def __init__(self, label: str, data_node, default_params: Optional[dict[str, str]]=None, **node_params):
+        super().__init__(label, data_node, **node_params)
 
         self._add_input_attribute(InputNodeAttribute("train dataset", self))
+        if not default_params: 
+            default_params = {key:list(choices.keys())[0] for key, choices in TrainParamsNode.__params.items()}
+        
+        def get_default(value):
+            return default_params.get(value, list(TrainParamsNode.__params[value].keys())[0])
+        
+        train_params: tuple[dict[str, object]] = (
+            {"label":"Project name", "type":'text', "default_value":default_params.get('Project name','Project'), "width":TrainParamsNode.WIDTH},
+            {"label":"Task name", "type":'text', "default_value":default_params.get('Task name', 'Experiment'), "width":TrainParamsNode.WIDTH},
+            {
+                "label":"Loss", "type":'combo', "default_value":get_default('Loss'), 
+                "items":tuple(TrainParamsNode.__params['Loss'].keys()), "user_data":TrainParamsNode.__params['Loss'], "width":TrainParamsNode.WIDTH
+            },
+            {
+                "label":"Optimizer", "type":'combo', "default_value":get_default('Optimizer'), 
+                "items":tuple(TrainParamsNode.__params['Optimizer'].keys()), "user_data":TrainParamsNode.__params['Optimizer'], "width":TrainParamsNode.WIDTH
+            },
+            {
+                "label":"Initialization", "type":'combo', "default_value":get_default('Initialization'), 
+                "items":tuple(TrainParamsNode.__params['Initialization'].keys()), "user_data":TrainParamsNode.__params['Initialization'], "width":TrainParamsNode.WIDTH
+            },
+            {"label":"Learning Rate", "type":'float', "default_value":default_params.get('Learning Rate', 0.05), "width":TrainParamsNode.WIDTH},
+            {"label":"Max Epoches", "type":'int', "default_value":default_params.get('Max Epoches', 2), "width":TrainParamsNode.WIDTH},
+            {"label":"Save Weights", "type":"file", "callback":Pipeline.save_weight},
+            {"label":"Load Weights", "type":"file", "callback":Pipeline.load_weight},
+            {"label":"Train", "type":"button", "callback":Pipeline.flow, "user_data":data_node},
+            {"label":"Continue Train", "type":"button", "callback":Pipeline.keep_train, "user_data":data_node},
+        )
         self._add_params(train_params)
-
-
+    
+    
+    
     def set_pipline(self, pipeline):
         self.pipeline = pipeline
 
